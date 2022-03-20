e820d1c546159b466d10a6d8398bdd5fcffa03fd
Rashid Mamarimov <sonochiwaxd@gmail.com>
2022-03-20 17:52:49 +0600

    mnoga sdelal

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ cookiesOkBtn.onclick = function () {
     setTimeout(deleteBlock, 600);
 };
 
-// setTimeout(showBlock, 1000);
+setTimeout(showBlock, 1000);
 
 
 // form error
@@ -30,7 +30,6 @@ sendBtn.onclick = function () {
     errorArea.classList.add('error');
 };
 
-
 // our-mission starts
 let missionText = document.querySelector('.our-mission__text');
 let enabledPhone = document.querySelector('.our-mission__phone');
@@ -39,10 +38,12 @@ function deleteTextFunc() {
     missionText.remove(missionText);
 }
 
-$(window).scroll(function () {
-    if ($(this).scrollTop() > $(missionText).offset().top - 500) {
-        missionText.classList.add('text-hidden');
-        enabledPhone.classList.add('phone-enabled');
-        setTimeout(deleteTextFunc, 3000);
-    }
-});
+if (screen.width > 1199) {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > $(missionText).offset().top - 500) {
+            missionText.classList.add('text-hidden');
+            enabledPhone.classList.add('phone-enabled');
+            setTimeout(deleteTextFunc, 3000);
+        }
+    });
+}
