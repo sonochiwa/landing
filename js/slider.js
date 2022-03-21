44309c909bedceb4bffe449b8507bfe44e085515
Rashid Mamarimov <sonochiwaxd@gmail.com>
2022-03-21 23:12:34 +0600

    teper tochno last

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,5 @@
 let next = document.querySelector(".next");
 let prev = document.querySelector(".prev");
-
 let slides = document.querySelector('.slides');
 let dots = document.querySelector('.dots');
 let item;
@@ -9,8 +8,8 @@ let scrolls = 0;
 let lastchild = slides.lastElementChild;
 let count = slides.children.length;
 let dotNumber = 0;
-slides.children[scrolls].getAttribute('value');
 
+slides.children[scrolls].getAttribute('value');
 dots.children[dotNumber].classList.add('active-dot');
 document.querySelector('.next').addEventListener('click', function () {
     next.disabled = true;
@@ -22,13 +21,11 @@ document.querySelector('.next').addEventListener('click', function () {
             slides.appendChild(item);
             slides.style.marginLeft = 0;
             next.disabled = false;
-
             for (let i = 0; i <= dots.children.length - 1; i++) {
                 dots.children[i].classList.remove('active-dot');
             }
             dotNumber = slides.children[scrolls].getAttribute('value');
             dots.children[dotNumber].classList.add('active-dot');
-
         } else {
             slides.style.marginLeft = parseInt(slides.style.marginLeft || 0) - 2 + 'px';
             x -= 2;
@@ -41,13 +38,11 @@ document.querySelector('.prev').addEventListener('click', function () {
     item = slides.children[scrolls];
     slides.insertBefore(slides.lastElementChild, item);
     slides.style.marginLeft = -400 + 'px';
-
     for (let i = 0; i <= dots.children.length - 1; i++) {
         dots.children[i].classList.remove('active-dot');
     }
     dotNumber = slides.children[scrolls].getAttribute('value');
     dots.children[dotNumber].classList.add('active-dot');
-
     let intervalMinus = setInterval(function () {
         if (x == 400) {
             clearInterval(intervalMinus);
